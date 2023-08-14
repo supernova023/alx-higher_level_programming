c087a08e6a4bc21bd7cf63e7663bbf33255613ee
Novashan Moodley <novashanm@gmail.com>
2023-08-13 20:05:07 -0700

    alx example 9 find max int value in list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list):
+    if len(my_list) == 0:
+        return (None)
+
+    max_val = my_list[0]
+    for x in range(len(my_list)):
+        if my_list[x] > max_val:
+            max_val = my_list[x]
+
+    return (max_val)
